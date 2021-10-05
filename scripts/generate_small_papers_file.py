84ad6f7e390cf23aeb845f44250c096263fabb7f
Kshitij Sinha <kshitij.avi.s@gmail.com>
2021-10-04 22:46:25 -0500

    Update source of data for embeddings
    Store_paper_embeddings.py now pulls data from json and csv files
    rather than SQL tables

---
+++ scripts/generate_small_papers_file.py
@@ -0,0 +1,20 @@
+"""Generate Small Paper File
+
+Script use to shrink the file 'filtered_arxiv.json' for testing purposes.
+Loads in JSON data and saves the first 'n' elements to a new file name 'filtered_arxiv_small.json'.
+Edit the variable new_paper_count to control the size of the output JSON file.
+"""
+
+import json
+
+new_paper_count = 1000
+paper_file = "../data/filtered_arxiv.json"
+save_file = "../data/filtered_arxiv_small.json"
+
+with open(paper_file) as f:
+    paper_json = json.load(f)
+
+sliced_json = paper_json[:new_paper_count]
+
+with open(save_file, "w") as f:
+    json.dump(sliced_json, f, indent=2)
--- src/store_paper_embeddings.py
+++ src/store_paper_embeddings.py
@@ -1,42 +1,35 @@
-# Code skeleton pulled from guidlines/keyword_assignment/store_paper_embeddings.py
-
-import sys
-import mysql.connector
+import json
 from sentence_transformers import SentenceTransformer
 
 from utils import write_pickle_data, concat_paper_info
 
 
-db = mysql.connector.connect(
-  host="localhost",
-  user="sandbox",
-  password="sandbox",
-  database="assign_paper_kwds"
-)
-cursor = db.cursor()
-
-print("Connected to database")
-
 data_directory = "../data/"
+
+# Raw data files
+papers_file = data_directory + "filtered_arxiv_small.json" # Generated using scripts/generate_small_papers_file.py
+keywords_file = data_directory + "Keywords-Springer-83K.csv"
+
+# Output pickle files
 emb_out_file = data_directory + "SB_paper_embeddings.pickle"
 idx_out_file = data_directory + "SB_paper_embedding_mag_ids.pickle"
 
-paper_count = 100
+paper_count = 50
 
-cursor.execute(f"""
-    SELECT id, title, abstract
-    FROM Publication
-    WHERE abstract IS NOT NULL
-    LIMIT {paper_count}
-""")
-paper_ts = cursor.fetchall()
+print("Loading paper file")
 
+with open(papers_file) as f:
+    paper_data = json.load(f)
+
+paper_ts = paper_data[:paper_count]
+
+print("Files loaded")
 
 print("Loading and preprocessing data/models")
 model = SentenceTransformer('bert-base-nli-mean-tokens')
 
-mag_ids = [t[0] for t in paper_ts]
-paper_raw = [concat_paper_info(t[1], t[2]) for t in paper_ts]
+mag_ids = [t['id'] for t in paper_ts]
+paper_raw = [concat_paper_info(t['title'], t['abstract']) for t in paper_ts]
 
 print("Getting embeddings for " + str(len(paper_raw)) + " papers")
 paper_embeddings = model.encode(paper_raw, show_progress_bar=True)
