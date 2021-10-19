df9d701f3e0d40e3e7be86e02ad0670e7ee9ae2d
Kshitij Sinha <kshitij.avi.s@gmail.com>
2021-10-19 16:51:24 -0500

    Update pickle-json script to read from correct folder

--- scripts/keyword_paper_pickle_to_json.py
+++ scripts/keyword_paper_pickle_to_json.py
@@ -8,5 +8,5 @@ def pickleToJson(in_filepath, out_filepath):
     with open(out_filepath, "w") as f:
         json.dump(p, f, indent=2)
 
-pickleToJson("data/springer_keyword_data.pickle", "schemas/keyword_metadata.json")
-pickleToJson("data/SB_filtered_paper_data.pickle", "schemas/paper_metadata.json")
+pickleToJson("../data/springer_keyword_data.pickle", "../schemas/keyword_metadata.json")
+pickleToJson("../data/SB_filtered_paper_data.pickle", "../schemas/paper_metadata.json")
