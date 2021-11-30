9000c97e639ca0c12e655554805abb8ae2541500
Kshitij Sinha <kshitij.avi.s@gmail.com>
2021-11-30 16:36:10 -0600

    Create script to store keywords in elasticsearch

---
+++ src/store_keywords_es.py
@@ -0,0 +1,28 @@
+import csv
+from elasticsearch import Elasticsearch, helpers
+
+# Generator pattern for efficient bulk indexing
+def generate_keyword_data(index, keywords_file):
+    keys_to_extract = ['keyword', 'frequency']
+    with open(keywords_file, newline='') as f:
+        keyword_data = csv.DictReader(f, quotechar="|")
+
+        for i, keyword_entry in enumerate(keyword_data):
+            entry_to_store = {key: keyword_entry[key] for key in keys_to_extract}
+
+            if i % 10000 == 0:
+                print(f"On {i}th keyword")
+
+            yield {
+                "_index": index,
+                "_source": entry_to_store
+            }
+
+    
+es = Elasticsearch()
+index = "fos"
+keywords_file = "../data/Keywords-Springer-83K.csv"
+
+print("Indexing keywords")
+# Indexing keywords
+helpers.bulk(es, generate_keyword_data(index, keywords_file))
