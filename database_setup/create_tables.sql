52f714aedd72f0bf54628638970d636ac19e890b
Kshitij Sinha <kshitij.avi.s@gmail.com>
2021-12-21 15:17:03 -0600

    Add schema for FoS_npmi_Springer table

--- database_setup/create_tables.sql
+++ database_setup/create_tables.sql
@@ -21,3 +21,15 @@ CREATE TABLE Publication_FoS (
     FOREIGN KEY (FoS_id) REFERENCES FoS(id) ON DELETE CASCADE,
     CONSTRAINT publication_keyword_pk PRIMARY KEY (publication_id, FoS_id)
 );
+
+CREATE TABLE FoS_npmi_Springer (
+    id1 int NOT NULL,
+    id2 int NOT NULL,
+    len1 int DEFAULT NULL,
+    len2 int DEFAULT NULL,
+    intersection_len int DEFAULT NULL,
+    npmi double DEFAULT NULL,
+    PRIMARY KEY (id1,id2),
+    FOREIGN KEY (id1) REFERENCES FoS (id),
+    FOREIGN KEY (id2) REFERENCES FoS (id)
+);
