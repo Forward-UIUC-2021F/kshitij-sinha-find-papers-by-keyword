3e672e48d7c2b75e92bb70c88d3ad56b7784cd3a
Kshitij Sinha <kshitij.avi.s@gmail.com>
2021-12-21 02:22:32 -0600

    Write scripts to handle MAG data

---
+++ scripts/generate_paper_embeddings.py
@@ -0,0 +1,16 @@
+from src.find_papers_by_keyword.embeddings_generator import EmbeddingsGenerator
+from src.file_readers.paper_file_reader import PaperFileReader
+from src.find_papers_by_keyword.utils import write_pickle_data
+
+paper_file = "data/mag_papers.json"
+reader = PaperFileReader("id", "title", "abstract", "n_citation")
+papers = reader.read_file(paper_file)
+
+print(papers[0])
+
+generator = EmbeddingsGenerator()
+embs, id_to_ind = generator.generate_paper_embeddings(papers)
+
+write_pickle_data(embs, "data/mag_embs.pickle")
+write_pickle_data(id_to_ind, "data/mag_id_to_ind.pickle")
+
