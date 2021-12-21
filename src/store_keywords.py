6d4f70f4ce812e65979295c165724f975390a3a0
Kshitij Sinha <kshitij.avi.s@gmail.com>
2021-12-21 13:21:28 -0600

    Add ability to store keyword embeddings

--- src/store_keywords.py
+++ src/store_keywords.py
@@ -1,5 +1,7 @@
 from src.file_readers.keyword_file_reader import KeywordFileReader
 from src.find_papers_by_keyword.database import Database
+from src.find_papers_by_keyword.embeddings_generator import EmbeddingsGenerator
+from src.find_papers_by_keyword.utils import write_pickle_data
 import mysql.connector
 import sql_creds
 
@@ -10,10 +12,16 @@ def main():
     parser = argparse.ArgumentParser(
         description="""
             Command-line utility for storing keyword-data into SQL database.
+
+            Takes keyword data in the form of a CSV and saves the
+            data to the MySQL database. Also generates keyword embeddings as saves
+            them to a pickle file
         """
     )
     parser.add_argument('keyword_file', type=str,
                         help='filepath to csv containing keyword data')
+    parser.add_argument('keyword_embs_outfile', type=str,
+                        help='filepath specifying destiation of keyword embeddings pickle file')
 
     args = parser.parse_args()
     print("Loading and parsing files...")
@@ -29,6 +37,11 @@ def main():
 
     database = Database(db_connection)
     database.store_keyword_data(keyword_data)
+
+    embeddings_generator = EmbeddingsGenerator()
+    keyword_embs = embeddings_generator.generate_keyword_embeddings(keyword_data)
+
+    write_pickle_data(keyword_embs)
 
     db_connection.close()
 
