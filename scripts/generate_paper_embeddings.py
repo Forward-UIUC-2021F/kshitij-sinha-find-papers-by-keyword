f2fc81353337ea33a8a7464c88133d4a28f0c7e1
Kshitij Sinha <kshitij.avi.s@gmail.com>
2021-12-21 02:50:14 -0600

    Update script to store json instead of pickle

--- scripts/generate_paper_embeddings.py
+++ scripts/generate_paper_embeddings.py
@@ -1,6 +1,6 @@
 from src.find_papers_by_keyword.embeddings_generator import EmbeddingsGenerator
 from src.file_readers.paper_file_reader import PaperFileReader
-from src.find_papers_by_keyword.utils import write_pickle_data
+from src.find_papers_by_keyword.utils import write_pickle_data, write_json_data
 
 paper_file = "data/mag_papers.json"
 reader = PaperFileReader("id", "title", "abstract", "n_citation")
@@ -12,5 +12,5 @@ generator = EmbeddingsGenerator()
 embs, id_to_ind = generator.generate_paper_embeddings(papers)
 
 write_pickle_data(embs, "data/mag_embs.pickle")
-write_pickle_data(id_to_ind, "data/mag_id_to_ind.pickle")
+write_json_data(id_to_ind, "data/mag_id_to_ind.json")
 
